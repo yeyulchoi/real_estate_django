f2e2f091e1b9c7bf55d14d0b9944a272e138c062
Yeyul Choi <yeyulchoi@outlook.com>
2024-10-15 02:15:09 -0400

    added listing models

--- real_estate_project/settings.py
+++ real_estate_project/settings.py
@@ -80,8 +80,11 @@ WSGI_APPLICATION = 'real_estate_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'realestatedb',
+        'USER':'postgres',
+        'PASSWORD':'Yoonja7979!!',
+        'HOST':'localhost'
     }
 }
 
